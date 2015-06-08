2cbcc3465c27e82cc0d6e3f17d0e18f606302aa2
Wagner Macedo <wagnerluis1982@gmail.com>
2015-06-08 13:20:10 -0300

    Support for subfigures

--- superfilter.py
+++ superfilter.py
@@ -12,6 +12,20 @@ def parse_options(s):
         return dict([re.split(r' *= *', opt) for opt in opts])
 
 
+def latex_join(values, sep):
+    output = [values[0]]
+    for i in range(1, len(values)):
+        last = output[-1]
+        curr = values[i]
+        if last['t'] == curr['t'] == "RawBlock" \
+                and last['c'][0] == curr['c'][0] == "latex":
+            output[-1] = latex(last['c'][1] + sep + curr['c'][1])
+        else:
+            output.append(values[i])
+
+    return output
+
+
 def latex(s):
     return pf.RawBlock('latex', s)
 
@@ -59,6 +73,24 @@ def put_figure(uri, caption):
                 latex(r'\end{figure%s}' % star)]
 
 
+def put_subfigures(images):
+    output = []
+    options = {'width': '1'}
+    for (uri, caption) in images:
+        subopts = {'width': '1'}
+        uri_has_args = re.findall(r'^(.*?)(?:%20)*\|(?:%20)*(.*)$', uri)
+        if uri_has_args:
+            uri, args = uri_has_args[0]
+            subopts = parse_options(args)
+
+        output.extend([latex(r'\begin{subfigure}[b]{%s\linewidth}' '\n'
+                             % subopts['width']),
+                       put_image(uri, options),
+                       put_caption(caption),
+                       latex(r'\end{subfigure}')])
+    return latex_join(output, '\n')
+
+
 def split(s, sep, maxsplit):
     explode = s.split(sep, maxsplit)
     while len(explode) <= maxsplit:
@@ -68,9 +100,10 @@ def split(s, sep, maxsplit):
 
 def do_filter(k, v, f, m):
     if k == "Para":
+        value = pf.stringify(v)
+
+        # Colunas no slide
         if f == 'beamer':
-            # Colunas no slide
-            value = pf.stringify(v)
             if not _.is_columns:
                 # Inicia bloco 'columns'
                 if value == '<[columns]':
@@ -85,6 +118,27 @@ def do_filter(k, v, f, m):
                 m = re.match(r'^\[\[\[ *(%s) *\]\]\]$' % RE_FLOAT, value)
                 if m:
                     return latex(r'\column{%s\textwidth}' % m.group(1))
+
+        # Subfiguras
+        if not _.is_figure:
+            if value == '<[figures]':
+                _.is_figure = True
+                return latex(r'\begin{figure}[ht]' '\n' r'\centering')
+        else:
+            if value == '[figures]>':
+                _.is_figure = False
+                return latex(r'\end{figure}')
+            if v[0]['t'] == "Str" and v[0]['c'] == "Caption:" \
+                    and v[1]['t'] == "Space":
+                return put_caption(v[2:])
+            if len(v) >= 1 and v[0]['t'] == "Image":
+                images = []
+                for i in range(len(v)):
+                    if v[i]['t'] == "Image":
+                        uri = v[i]['c'][1][0]
+                        caption = v[i]['c'][0]
+                        images.append((uri, caption))
+                return put_subfigures(images)
 
         # Imagens com largura informada
         if len(v) == 1 and v[0]['t'] == "Image":
@@ -142,6 +196,7 @@ def do_filter(k, v, f, m):
 # flags
 _ = do_filter
 _.is_columns = False
+_.is_figure = False
 
 
 if __name__ == "__main__":
